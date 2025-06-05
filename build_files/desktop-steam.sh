7461a3e810588c6c43633e4eb28606ac59b7e8b3
Hec <hec@heccraft.com>
2025-06-05 16:14:41 -0400

    adjust workflow and add desktop-steam.sh for test

---
+++ build_files/desktop-steam.sh
@@ -0,0 +1,26 @@
+#!/usr/bin/bash
+
+set -eoux pipefail
+
+# OBS-VKcapture
+dnf5 -y copr enable kylegospo/obs-vkcapture
+
+# Bazzite Repos
+dnf5 -y copr enable kylegospo/bazzite
+dnf5 -y copr enable kylegospo/bazzite-multilib
+dnf5 -y copr enable kylegospo/LatencyFleX
+
+find /etc/yum.repos.d/
+
+sed -i "0,/enabled=0/{s/enabled=0/enabled=1/}" /etc/yum.repos.d/negativo17-fedora-multimedia.repo
+
+# TODO: pull the following scripts directly from m2os
+/ctx/build_files/steam.sh
+
+sed -i "s@enabled=1@enabled=0@" /etc/yum.repos.d/negativo17-fedora-multimedia.repo
+
+# disable the Repos we pulled in above
+dnf5 -y copr disable kylegospo/obs-vkcapture
+dnf5 -y copr disable kylegospo/bazzite
+dnf5 -y copr disable kylegospo/bazzite-multilib
+dnf5 -y copr disable kylegospo/LatencyFleX 

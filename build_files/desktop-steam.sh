2c292f4919c3feeeb40bbeb2488c6eceb8c5a597
Hec <hec@heccraft.com>
2025-07-29 18:38:05 -0400

    add cleanup workflow

--- build_files/desktop-steam.sh
+++ build_files/desktop-steam.sh
@@ -14,7 +14,6 @@ find /etc/yum.repos.d/
 
 sed -i "0,/enabled=0/{s/enabled=0/enabled=1/}" /etc/yum.repos.d/negativo17-fedora-multimedia.repo
 
-# TODO: pull the following scripts directly from m2os
 /ctx/steam.sh
 
 sed -i "s@enabled=1@enabled=0@" /etc/yum.repos.d/negativo17-fedora-multimedia.repo
